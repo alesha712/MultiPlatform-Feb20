50ed3c6719bbc8ba65e74070843598e3bdf9b2af
Arie Peretz <ariep20@gmail.com>
2020-02-18 07:46:30 +0200

    label is now from storyboard - iOS project

--- KotlinMPP/native/KotlinIOS/KotlinIOS/ViewController.swift
+++ KotlinMPP/native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -12,18 +12,11 @@ import SharedModule
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        
-        
-        label.font = label.font.withSize(25)
-        label.text = CommonKt.createApplicationScreenMessage()
-        view.addSubview(label)
+        lblTitle.text = CommonKt.createApplicationScreenMessage()
     }
 
 
