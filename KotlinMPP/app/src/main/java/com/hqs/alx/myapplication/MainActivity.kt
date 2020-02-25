cd82813cad7f5ceeaeaf324a1c99c6114f92b4a7
Alex Istoyler <34527088+alesha712@users.noreply.github.com>
2020-02-25 13:13:24 +0200

    Finish repos and cache manager

--- KotlinMPP/app/src/main/java/com/hqs/alx/myapplication/MainActivity.kt
+++ KotlinMPP/app/src/main/java/com/hqs/alx/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import com.hqs.alx.sharedmodule.repo.ConversionsRepo
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -18,17 +19,15 @@ class MainActivity : AppCompatActivity() {
 
         tvMain.text = createApplicationScreenMessage()
 
-        for (i in 1..10) {
-            ConversionsRepo().convertRates("USD", "ILS", "5.0") {
-                CoroutineScope(Dispatchers.Main).launch {
-                    Log.d("alex", "this is before coroutines")
-                    try {
+        ConversionsRepo().convertRates("USD", "ILS", "5.0") {
+            CoroutineScope(Dispatchers.Main).launch {
+                Log.d("alex", "this is before coroutines")
+                try {
 //                    Log.d("alex", "getConversions success: ${it.rates}")
 //                    Log.d("alex", "getConversions size: ${it.rates?.keys?.size}")
-                        Log.d("alex", "list size: ${it.result}")
-                    } catch (e: Exception) {
-                        Log.d("alex", "getConversions success: error")
-                    }
+                    Log.d("alex", "list size: ${it.result}")
+                } catch (e: Exception) {
+                    Log.d("alex", "getConversions error: ${e.message}")
                 }
             }
         }
